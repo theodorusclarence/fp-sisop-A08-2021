2c35c80a99b24bb3e92d199d91686ae62cdc4955
Clarence <theodorusclarence@gmail.com>
2021-06-14 21:12:27 +0700

    feat: temporary file structure

---
+++ clarence-client.c
@@ -0,0 +1,85 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+void promptDataManipulation(int sock);
+void promptTable(int sock, char *str);
+void promptDatabase(int sock, char *str);
+
+int main(int argc, char const *argv[]) {
+  struct sockaddr_in address;
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  char *hello = "Hello from client";
+  char buffer[1024] = {0};
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+
+  memset(&serv_addr, '0', sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+
+  while (1) {
+    promptDataManipulation(sock);
+  }
+
+  return 0;
+}
+
+void promptDataManipulation(int sock) {
+  char command[100];
+  printf("⏰ waiting promptDataManipulation\n");
+  scanf("%s", command);
+
+  if (!strcmp(command, "CREATE")) {
+    char type[100];
+    scanf("%s", type);
+
+    if (!strcmp(type, "TABLE")) {
+      char tableName[100];
+      scanf("%s", tableName);
+      send(sock, "table", strlen("table"), 0);
+      sleep(0.2);
+      promptTable(sock, tableName);
+    } else if (!strcmp(type, "DATABASE")) {
+      char dbName[100];
+      scanf("%s", dbName);
+      send(sock, "database", strlen("database"), 0);
+      sleep(0.2);
+      promptDatabase(sock, dbName);
+    }
+    // promptDatabase(sock);
+  }
+};
+
+void promptTable(int sock, char *str) {
+  // ? Remove `;`
+  str[strlen(str) - 1] = '\0';
+
+  send(sock, str, strlen(str), 0);
+}
+
+void promptDatabase(int sock, char *str) {
+  // ? Remove `;`
+  str[strlen(str) - 1] = '\0';
+
+  send(sock, str, strlen(str), 0);
+}
---
+++ clarence.c
@@ -0,0 +1,98 @@
+
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#define PORT 8080
+#define SIZE 1024
+
+void handleDatabase(int sock);
+void handleTable(int sock);
+
+int main(int argc, char const *argv[]) {
+  // CREATE FILES FOLDER
+  // mkdir("FILES", 0777);
+
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+
+  printf("running on port %d\n", PORT);
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // ?  INI CODE ASLI, di mac SO_REUSEPORT harus diapus
+  // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+  //                sizeof(opt))) {
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_t tid[50];
+
+  while (1) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+
+    int valread;
+    char buffer[1024] = {0};
+    valread = read(new_socket, buffer, 1024);
+    printf("🚀 [main()] first command: %s\n", buffer);
+
+    if (!strcmp(buffer, "database")) {
+      handleDatabase(new_socket);
+    } else if (!strcmp(buffer, "table")) {
+      handleTable(new_socket);
+    }
+  }
+
+  return 0;
+}
+
+void handleDatabase(int sock) {
+  // WAITING FOR MENU
+  int valread;
+  char buffer[1024] = {0};
+  valread = read(sock, buffer, 1024);
+  printf("🚀 [handleDatabase()] first command: %s\n", buffer);
+  return;
+}
+
+void handleTable(int sock) {
+  // WAITING FOR MENU
+  int valread;
+  char buffer[1024] = {0};
+  valread = read(sock, buffer, 1024);
+  printf("🚀 [handleTable()] first command: %s\n", buffer);
+  return;
+}
